1e356df4ab6f593f76f2851f103b8fcafefd1753
DevVithor <vithor.mitsuyoshi@superfestval.com.br>
2024-05-16 11:00:13 -0300

    refactor: remove try and catch on CreatePlan

--- src/infra/controllers/plans/create/CreatePlanController.ts
+++ src/infra/controllers/plans/create/CreatePlanController.ts
@@ -6,15 +6,11 @@ export class CreatePlanController {
 
     async execute(req: Request, res: Response) {
 
-        try {
+        const data = req.body
 
-            const data = req.body
+        const createPlan = await this.createPlan.execute(data)
+        res.status(201).json(createPlan)
 
-            const createPlan = await this.createPlan.execute(data)
-            res.status(201).json(createPlan)
 
-        } catch (error) {
-            console.log(error)
-        }
     }
 }
