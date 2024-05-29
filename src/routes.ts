07fae1789df501417918efb0ff11165dfefb9584
DevVithor <vithor.mitsuyoshi@superfestval.com.br>
2024-05-29 16:59:46 -0300

    refactor: add on routes "next"

--- src/routes.ts
+++ src/routes.ts
@@ -1,48 +1,48 @@
-import express from "express"
-import { createPlanController } from "./infra/controllers/plans/create";
-import { findPlanController } from "./infra/controllers/plans/find";
-import { deletePlanController } from "./infra/controllers/plans/delete";
-import { createSubscriptionsController } from "./infra/controllers/subscriptions/create";
-import { createPaymentCardController } from "./infra/controllers/payment/create";
-import { updatePlanController } from "./infra/controllers/plans/update";
-import { findSubscriptionController } from "./infra/controllers/subscriptions/find";
+import express, { NextFunction, Request, Response } from "express"
 import { createUserController } from "./infra/controllers/user";
-import { authLoginController } from "./infra/controllers/login/create";
-import { getUserController } from "./infra/controllers/login/find";
 import { authMiddleware } from "./core/middleware/authMiddleware";
+import { getUserController } from "./infra/controllers/login/find";
+import { findPlanController } from "./infra/controllers/plans/find";
+import { authLoginController } from "./infra/controllers/login/create";
+import { createPlanController } from "./infra/controllers/plans/create";
+import { deletePlanController } from "./infra/controllers/plans/delete";
+import { updatePlanController } from "./infra/controllers/plans/update";
+import { createPaymentCardController } from "./infra/controllers/payment/create";
+import { findSubscriptionController } from "./infra/controllers/subscriptions/find";
+import { createSubscriptionsController } from "./infra/controllers/subscriptions/create";
 
 const router = express.Router()
 
 router
-    .get("/v1/plans", (req, res) =>
-        findPlanController.execute(req, res)
+    .get("/v1/plans", (req, res, next) =>
+        findPlanController.execute(req, res, next)
     )
-    .get("/find/subscription", (req, res) =>
-        findSubscriptionController.execute(req, res)
+    .get("/find/subscription", (req, res, next) =>
+        findSubscriptionController.execute(req, res, next)
     )
-    .post("/plans/create/", (req, res) =>
-        createPlanController.execute(req, res)
+    .post("/plans/create/", (req, res, next) =>
+        createPlanController.execute(req, res, next)
     )
-    .post("/v1/plan/:external_id/subscription", (req, res) =>
-        createSubscriptionsController.execute(req, res)
+    .post("/v1/plan/:external_id/subscription", (req, res, next) =>
+        createSubscriptionsController.execute(req, res, next)
     )
-    .post("/v1/subscription/:subscription_id/pay", (req, res) =>
-        createPaymentCardController.execute(req, res)
+    .post("/v1/subscription/:subscription_id/pay", (req, res, next) =>
+        createPaymentCardController.execute(req, res, next)
     )
-    .post("/register", (req, res) => {
-        createUserController.execute(req, res)
+    .post("/register", (req, res, next) => {
+        createUserController.execute(req, res, next)
     })
-    .post("/login", (req, res) => {
-        authLoginController.execute(req, res)
+    .post("/login", (req, res, next) => {
+        authLoginController.execute(req, res, next)
     })
-    .get("/profile", authMiddleware, (req, res) => {
-        getUserController.execute(req, res)
+    .get("/profile", authMiddleware, (req, res, next) => {
+        getUserController.execute(req, res, next)
     })
-    .delete("/v1/plan/:id", (req, res) =>
-        deletePlanController.execute(req, res)
+    .delete("/v1/plan/:id", (req, res, next) =>
+        deletePlanController.execute(req, res, next)
     )
-    .put("/v1/plan/:external_id", (req, res) =>
-        updatePlanController.execute(req, res)
+    .put("/v1/plan/:external_id", (req, res, next) =>
+        updatePlanController.execute(req, res, next)
     )
 
 export default router;
