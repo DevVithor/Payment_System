802405d6b959cc24fd954798263a54dfbe184378
DevVithor <vithor.mitsuyoshi@superfestval.com.br>
2024-06-05 16:36:50 -0300

    refactor: update middleware error

--- src/core/middleware/error.ts
+++ src/core/middleware/error.ts
@@ -1,11 +1,15 @@
 import { Request, Response, NextFunction } from 'express'
 import ErrorHandler from '../../handler/error/ErrorHandler'
+import { ZodError } from 'zod'
 
 export const errorMiddleware = (error: Error & Partial<ErrorHandler>, req: Request, res: Response, next: NextFunction) => {
 
     {
         const statusCode = error.statuscode || 500
-        const message = error.statuscode && error.message
+        const message = error.message
+        if (error instanceof ZodError) {
+            res.status(400).json(error.errors)
+        }
         res.status(statusCode).json({ message })
     }
 
